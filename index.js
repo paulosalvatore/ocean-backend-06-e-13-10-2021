370512ec56a5d37026949aa1c1a48051d994a99d
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-06 11:30:30 -0300

    Endpoint Read All criado

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 
+// Endpoints de 'Hello'
+
 app.get("/", function (req, res) {
     res.send("Hello, World!");
 });
 
 app.get("/oi", function (req, res) {
     res.send("Olá, mundo!");
+});
+
+// Lista de heróis
+
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+
+// Endpoint de Read All
+
+app.get("/herois", function (req, res) {
+    res.send(lista);
 });
 
 app.listen(3000);
